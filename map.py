62da9b7d47430b9cb1fae83fcac5df03d4c9bea4
tirtha <tsark99@gmail.com>
2017-04-01 00:43:56 +0530

    Added tiles selector

--- map.py
+++ map.py
@@ -1,20 +1,28 @@
 import folium
 import pandas
-map= folium.Map(location=[48.1118011,-121.1110001],zoom_start= 4, tiles="Stamen Terrain")
-df=pandas.read_csv("Sample_Data_Volcano_USA.txt")
+import datetime
+data=input("Please enter the csv file name with the correct extension\n")
+df=pandas.read_csv(data)
+titles=["OpenStreetMap", "Mapbox Bright", "Mapbox Control Room", "Stamen Terrain", "CartoDB dark_matter"]
+i=int(input("Enter the serial Number for tiles to use for the map:\n 1.OpenStreetMap\n 2.Mapbox Bright\n 3. Mapbox Control Room\n 4.Stamen Terrain\n 5.CartoDB dark_matter\n"))
+LAT=input("Please enter the column head name for latitude")
+LAT=input("Please enter the column head name for longitude")
+map= folium.Map(location=[df[LAT].mean(),df[LON].mean()],zoom_start= 4, tiles=titles[i-1])
+
 minimum= int(min(df['ELEV']))
 step= int((max(df['ELEV'])-minimum)/3)
 maximum=int(max(df["ELEV"]))
+
 def color(elev):
     if elev in range(minimum,step+minimum):
         return "red"
-
     elif elev in range(step+minimum,2*step+minimum):
         return "green"
     elif elev in range (2*step+minimum,maximum):
         return "blue"
-for lat,lon,name,elev in zip(df["LAT"],df["LON"],df["NAME"],df['ELEV']):
+
+for lat,lon,name,elev in zip(df[LAT],df[LON],df["NAME"],df['ELEV']):
             folium.Marker([lat,lon,], popup=name, icon = folium.Icon(color =color(elev),icon_color="yellow")).add_to(map)
 
-#folium.CircleMarker([22.5867,88.4171],radius=150, popup='SALTLAKE CITY',fill_color='#3186cc').add_to(map)
-map.save("map.html")
+
+map.save(("Map("+datetime.datetime.now().strftime(" %Y-%m-%d-%H-%M-%S")+").html"))
